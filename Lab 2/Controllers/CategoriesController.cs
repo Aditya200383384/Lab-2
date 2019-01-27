bd38bae5a5ee79666532089342c38b77cfec20c3
Aditya200383384 <46662250+Aditya200383384@users.noreply.github.com>
2019-01-26 22:31:45 -0500

    Added controller/views.

---
+++ Lab 2/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(String Category)
+        {
+            ViewBag.Category = "Your Selected Category : " + Category;
+            return View();
+        }
+    }
+}
